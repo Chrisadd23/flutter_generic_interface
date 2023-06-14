19358534286434ab8607a738632ae1ccf6da386f
Christopher Haas <christopherhaas@MBPvonCistopher.fritz.box>
2023-06-14 17:26:05 +0200

    feat: create flutter project

---
+++ android/app/src/main/kotlin/com/example/flutter_generic_interface/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_generic_interface
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_test_generic_interface/android/app/src/main/kotlin/com/example/flutter_test_generic_interface/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_test_generic_interface
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
